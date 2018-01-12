f9db0cb4a023fdc092ec8aced1cafc8108e40fe7
bruce-zxy <bruce_zxy@163.com>
2018-01-12 14:05:49 +0800

    page one

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ var mySwiper = new Swiper ('.swiper-container', {
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev'
 })
-$(".panel-heading").click(function(e){
+$(".panel-heading").click(function (e){
     /*切换折叠指示图标*/
     $(this).find("span").toggleClass("glyphicon-chevron-down");
     $(this).find("span").toggleClass("glyphicon-chevron-up");
 });
+
+
+$("#divAd1, #divAd2").click(function (e) {
+    this.style.display = "none";
+})
